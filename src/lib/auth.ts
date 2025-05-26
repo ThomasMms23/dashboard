5c5c7440a1d802b934363767d7fff63a76ca65eb
Thomas <monmousseau.thomas@gmail.com>
2025-05-26 17:20:22 +0200

    fix(auth): remove runtime ENV checks to fix build

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,17 +7,12 @@ const googleClientId = process.env.AUTH_GOOGLE_ID;
 const googleClientSecret = process.env.AUTH_GOOGLE_SECRET;
 const authSecret = process.env.AUTH_SECRET;
 
-if (!googleClientId) throw new Error("AUTH_GOOGLE_ID is not defined in .env");
-if (!googleClientSecret)
-  throw new Error("AUTH_GOOGLE_SECRET is not defined in .env");
-if (!authSecret) throw new Error("AUTH_SECRET is not defined in .env");
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     Google({
-      clientId: googleClientId,
-      clientSecret: googleClientSecret,
+      clientId: googleClientId ?? "",
+      clientSecret: googleClientSecret ?? "",
     }),
   ],
   session: { strategy: "jwt" },
@@ -32,5 +27,5 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
   },
   pages: { signIn: "/login" },
-  secret: authSecret,
+  secret: authSecret ?? "",
 });
